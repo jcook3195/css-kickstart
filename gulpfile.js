7b577994399190251cc403e95710c3da6f26b08b
Jordan Cook <jordan.cook@telrite.com>
2023-03-20 21:10:50 -0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+'use strict';
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('node-sass'));
+gulp.task('sass', function () {
+    return gulp.src('./scss/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+});
